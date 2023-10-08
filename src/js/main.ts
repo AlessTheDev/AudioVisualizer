b5af87950d0587d4b6e75bcb4e9d506467078cb6
Alessio <aless.cal06@gmail.com>
2023-10-08 19:46:16 +0200

    backup

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,14 +5,11 @@ import Scene from './objects/Scene.js'
 import AudioBars from './objects/AudioBars.js';
 import AudioAnalyzer from './AudioAnalyzer.js';
 import FrequencyBand from './FrequencyBand.js';
+import RythmicCircle from './objects/RythmicCircle.js';
 
 //Audio 
 let audio = new Audio();
 audio.volume = 1;
-
-let audioBars: AudioBars;
-
-let audioAnalyzer: AudioAnalyzer
 
 const file: any = document.getElementById("file-upload");
 file?.addEventListener("change", () => {
@@ -22,10 +19,31 @@ file?.addEventListener("change", () => {
 
   audio.volume = 0.5;
 
+
+  let frequencyBands = [
+    new FrequencyBand(0, 86),
+    new FrequencyBand(87, 172),
+    new FrequencyBand(173, 344),
+    new FrequencyBand(345, 688),
+    new FrequencyBand(689, 1376),
+    new FrequencyBand(1377, 2752),
+    new FrequencyBand(2753, 5504),
+    new FrequencyBand(5505, 11008),
+  ]
+
+  initAudioBars();
+  initMusicCircle(frequencyBands);
+})
+
+function initAudioBars() {
   const audioContext = new window.AudioContext();
 
-  //AudioBars
-  let frequencyBands: FrequencyBand[] = [
+  let audioBars: AudioBars;
+
+  let audioAnalyzer: AudioAnalyzer
+
+  // AudioBars frequency bands
+  const frequencyBands: FrequencyBand[] = [
     new FrequencyBand(2753, 5504),
     new FrequencyBand(10, 86),
     new FrequencyBand(60, 250),     // Bass
@@ -48,7 +66,18 @@ file?.addEventListener("change", () => {
   SceneManager.instance?.activeScene?.addObject(audioAnalyzer);
 
   SceneManager.instance?.activeScene?.addObject(audioBars);
-})
+}
+function initMusicCircle(frequencyBands: FrequencyBand[]) {
+  const audioContext = new window.AudioContext();
+
+  let audioAnalyzer = new AudioAnalyzer(audio, audioContext, frequencyBands, false);
+
+  let musicCircle = new RythmicCircle(100, innerHeight / 2, 50, "../img/cat.png");
+
+  SceneManager.instance?.activeScene?.addObject(audioAnalyzer);
+
+  SceneManager.instance?.activeScene?.addObject(musicCircle);
+}
 
 //Scene manager initialization
 const sceneManager = new SceneManager();
--- src/js/objects/RythmicCircle.ts
+++ src/js/objects/RythmicCircle.ts
@@ -1,6 +1,31 @@
+import GameObject from "./GameObject";
+import SceneManager from "./SceneManager";
 import SimpleGameObject from "./SimpleGameObject";
 
-export default class RythmicCircle extends SimpleGameObject{
-    
+export default class RythmicCircle extends GameObject {
+    private image: HTMLImageElement;
+    private size: number;
+
+    constructor(x: number, y: number, size: number, image: string) {
+        super(x, y, size, "transparent");
+        this.size = size;
+        this.disableCollider();
+        this.image = new Image();
+        this.image.src = image;
+    }
+    draw() {
+        console.log("test")
+        const c = SceneManager.instance?.activeScene?.c;
+        c?.beginPath();
+        c?.arc(this.x, this.y, this.hitboxRadius, 0, Math.PI * 2, false);
+        c?.fill();
+        c?.closePath();
+        c!.clip();
+        c!.drawImage(this.image, this.x, this.y, this.size, this.size);
+    }
+
+    update(){
+        this.draw();
+    }
 }
 
